186c077fc267609179e83be56957ecd334b23a92
cx201f <cx201f@gmail.com>
2020-04-16 16:57:42 +0800

    story2

--- s14day18/a1.py
+++ s14day18/a1.py
@@ -1,16 +1,21 @@
 # coding:utf-8
 from lxml import etree
 import requests
-
+import time
 file_name = 'text_new_'
-for kk in range(1, 200):
+
+
+for kk in range(1, 1000):
     ss_list = []
     url = 'http://guyeshanren2011.com/weibo/%E5%A7%91%E5%B0%84%E5%B1%B1%E4%BA%BA2011?page=' + str(kk)
-    url2 = 'http://guyeshanren2011.com/latestweibo?page=' + str(kk)
+    url2 = 'http://guyeshanren2011.com/weibo/%E5%A7%91%E5%B0%84%E5%B1%B1%E4%BA%BA2011theone?page=' + str(kk)
+
+    # http://guyeshanren2011.com/latestweibo?page=2
     print(url2)
 
-
+    # /html/body/div/a[1]/h4
     html = requests.get(url2).text    #这里一般先打印一下html内容，看看是否有内容再继续。
+    #time.sleep(1)
     s = etree.HTML(html)
     #title = s.xpath('//*[@id="content"]/div/div[1]/div/table[1]/tbody/tr/td[2]/div/a')
     for i in range(1,100):
