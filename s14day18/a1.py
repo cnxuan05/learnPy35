345d604786571e3f34e6320daa08f195d6f13411
cx201f <cx201f@gmail.com>
2020-04-16 15:17:40 +0800

    story2

---
+++ s14day18/a1.py
@@ -0,0 +1,61 @@
+# coding:utf-8
+from lxml import etree
+import requests
+
+file_name = 'text_new_'
+for kk in range(1, 200):
+    ss_list = []
+    url = 'http://guyeshanren2011.com/weibo/%E5%A7%91%E5%B0%84%E5%B1%B1%E4%BA%BA2011?page=' + str(kk)
+    url2 = 'http://guyeshanren2011.com/latestweibo?page=' + str(kk)
+    print(url2)
+
+
+    html = requests.get(url2).text    #这里一般先打印一下html内容，看看是否有内容再继续。
+    s = etree.HTML(html)
+    #title = s.xpath('//*[@id="content"]/div/div[1]/div/table[1]/tbody/tr/td[2]/div/a')
+    for i in range(1,100):
+        sstring = '/html/body/div/a[%s]/h4/text()' % str(i)
+        stime = s.xpath(sstring)
+        if not stime:
+            continue
+
+        sstring = '/html/body/div/div[%s]/p/text()' % str(i)
+        sbody = s.xpath(sstring)
+        if sbody:
+            pass
+            #print(sbody[0])
+
+        need_data = '%s###%s' % (stime, sbody)
+        #print(need_data)
+        ss_list.append(need_data)
+        new_file_name = file_name + str(kk).zfill(3)
+        with open(new_file_name, 'w') as fp:
+            for line in ss_list:
+                fp.write(line + '\n')
+
+# /html/body/div/div[2] /html/body/div/div[1]/p v/html/body/div/div[1]/p /html/body/div/div[2]/p/text()
+# /html/body/div/a[2]/h4 /html/body/div/a[1]/h4 v/html/body/div/div[1]/p/text() /html/body/div/div[8]/p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
