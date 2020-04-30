9670502f0461d32edffd8aeaf0de6e6b07c5eb10
cnxuan <cnxuan05@gmail.com>
2020-05-01 00:57:07 +0800

    create conf file 31

--- s15day01/yi/cc.py
+++ s15day01/yi/cc.py
@@ -1,63 +1,110 @@
-#coding:utf-8
+# coding:utf-8
 
 
 import random
-def a():
-
-    return random.randint(2,3)
-
-def b():
-    sum = 0
-    for i in range(3):
-        dd = a()
-        #print(dd)
-        sum += dd
-        pass
-    return sum
-
-def c():
-    sa = 0
-    s1 = []
-    s2 = []
-
-    def is_odd(n):
-        data = 0
-        if n % 2 == 0:
-            print(0)
-        else:
-            print(1)
-        #return n % 2 == 1
-    #
-    # newlist = filter(is_odd, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    # print(newlist)
 
 
+def generate_taichi():
+    return random.randint(2, 3)
+
+
+def generate_piece():
+    # pics = list()
+    sums = 0
+    sums += generate_taichi()
+    sums += generate_taichi()
+    sums += generate_taichi()
+    return sums
+
+
+def generate_gua():
+    pre_gua = list()
+    aft_gua = list()
     for i in range(6):
-        sss = b()
-        sa += sss
-
-        s1.append(sss)
-
-
-        if sss == 9:
-            s2.append(6)
-        elif sss == 8:
-            s2.append(9)
+        n_data = generate_piece()
+        pre_gua.append(n_data)
+        if 9 == n_data:
+            aft_gua.append(6)
+        elif 8 == n_data:
+            aft_gua.append(9)
         else:
-            s2.append(sss)
+            aft_gua.append(n_data)
+    # print(list(pre_gua))
+    # print(list(aft_gua))
+    print('#' * 6)
 
-    print('s1', s1)
-    print('s2', s2)
+    new_pre_gua = [0 if m % 2 == 0 else 1 for m in pre_gua]
+    get_sixth_eight(new_pre_gua)
 
-    newlist = filter(is_odd, s1)
-    for u in newlist:
-       print(u)
-    print('###')
-    newlist = filter(is_odd, s2)
-    for u in newlist:
-       print(u)
-    print(55 - sum(s1))
-    print(55 - sum(s2))
+    print(get_rank_num(55 - sum(pre_gua)))
+    print('#' * 6)
+
+    new_aft_gua = [0 if m % 2 == 0 else 1 for m in aft_gua]
+    get_sixth_eight(new_aft_gua)
+
+    print(get_rank_num(55 - sum(aft_gua)))
+    print('#' * 6)
     return
-d = c()
-print(d)
+
+
+def get_eight(p_list):
+    if p_list == [1, 1, 1]:
+        return '天'
+    if p_list == [0, 0, 0]:
+        return '地'
+    if p_list == [1, 0, 1]:
+        return '火'
+    if p_list == [0, 1, 0]:
+        return '水'
+    if p_list == [0, 1, 1]:
+        return '风'
+    if p_list == [1, 1, 0]:
+        return '泽'
+    if p_list == [1, 0, 0]:
+        return '雷'
+    if p_list == [0, 0, 1]:
+        return '山'
+
+
+def get_sixth_eight(q_list):
+    sum_info = [
+        '天天乾', '天风姤', '天山遁', '天地否', '风地观', '山地剥', '火地晋', '火天大有',
+        '水水坎', '水泽节', '水雷屯', '水火既济', '泽火革', '雷火丰', '地火明夷', '地水师',
+        '山山山', '山火贲', '山天大畜', '山泽损', '火泽睽', '天泽履', '风泽中孚', '风山渐',
+        '雷雷雷', '雷地豫', '雷水解', '雷风恒', '地风升', '水风井', '泽风大过', '泽雷随',
+        '风风风', '风天小畜', '风火家人', '风雷益', '天雷无妄', '火雷噬嗑', '山雷颐', '山风蛊',
+        '火火火', '火山旅', '火风鼎', '火水未济', '山水蒙', '风水涣', '天水讼', '天火同人',
+        '地地地', '地雷复', '地泽临', '地天泰', '雷天大壮', '泽天夬', '水天需', '水地比',
+        '泽泽泽', '泽水困', '泽地萃', '泽山咸', '水山蹇', '地山谦', '雷山小过', '雷泽归妹',
+
+    ]
+    lower = q_list[0:3]
+    upper = q_list[3:]
+    # print(lower)
+
+    print(get_eight(upper))
+    print(get_eight(lower))
+    for elem in sum_info:
+        if elem[0] == get_eight(upper) and elem[1] == get_eight(lower):
+            print(elem[2:])
+
+    return
+
+
+def get_rank_num(pnum):
+    if pnum <= 6:
+        return pnum
+    elif pnum <= 11 and pnum > 6:
+        return 12 - pnum
+    elif pnum <= 16 and pnum > 11:
+        return pnum - 10
+    elif pnum <= 21 and pnum > 16:
+        return 22 - pnum
+
+    return
+
+
+if __name__ == '__main__':
+    generate_gua()
+
+    pass
