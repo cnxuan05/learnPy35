76617162316bfa10466b604e18d6fcb8bc519abb
cnxuan05 <cnxuan05@gmail.com>
2021-04-23 08:16:38 +0800

    modify password

--- get_for.py
+++ get_for.py
@@ -15,18 +15,16 @@ mkdir -p /usr/local/oracle/data_temp  && chmod 777 /usr/local/oracle/data_temp
 docker run --privileged --restart always -d -p 8080:8080 -p 1521:1521 -v /home/newdata/:/data/ -v /home/newdata/data_temp/:/home/oracle/data_temp/  --name orac truevoly/oracle-12c /bin/bash /data/run.sh
 docker pull docker.io/truevoly/oracle-12c
 
-cd /
-
 
 sqlplus system/oracle@//localhost:1521/xe
 
+redis 6379 123456
 
+system/oracle
 
 
 
 '''
-
-
 
 
 
