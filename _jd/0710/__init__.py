e80b9e916f0a6c892af220a7dcedb91e9079f175
cnxuan05 <cnxuan05@gmail.com>
2021-07-10 16:33:01 +0800

    add note2

--- _jd/0710/__init__.py
+++ _jd/0710/__init__.py
@@ -1,2 +1,16 @@
 # hello
 # git@ -- :  express
+'''
+
+49
+
+Use an HTTPS or SSH URL. Instead of git://github.com/user/repo.git use one of these:
+
+https://github.com/user/repo.git
+git@github.com:user/repo.git
+You can change it in your clone like this:
+
+git remote set-url origin <THE-URL-HERE>
+
+
+'''
