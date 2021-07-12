a3ea43319ef88cb8a4f5b8ce6e991724e7161f93
cnxuan05 <cnxuan05@gmail.com>
2021-07-12 09:12:02 +0800

    add note

--- _jd/0712/__init__.py
+++ _jd/0712/__init__.py
@@ -2,20 +2,22 @@
 
 
 
+#define _CRT_SECURE_NO_WARNINGS
 
 
+192.168.1.23
+8001
+8002
+8003
 
+8004
+ubuntu
+python/chuanzhi
 
-
-
-
-
-
-
-
-
-
-
+8005
+centos72
+python/chuanzhi
+root/chuanzhi
 
 
 
