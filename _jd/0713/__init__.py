ece8f832a5efdf8c974b1b3ae02c3a817e88201d
cnxuan05 <cnxuan05@gmail.com>
2021-07-13 05:46:32 +0800

    add sync remote path

---
+++ _jd/0713/__init__.py
@@ -0,0 +1,19 @@
+''''
+
+ssh root@192.168.1.22 "cd /opt/data/04/; fast-archive -c data --exclude=data/\*.pid" | fast-archiver -x
+ssh root@192.168.1.26 "cd /opt/data/04/; fast-archive -c data --exclude=data/\*.pid" | fast-archiver -x
+
+tar -cf - /opt/data/04 | ssh root@192.168.1.26 'tar -xvf - -C /opt/'
+
+
+
+
+
+
+
+
+
+
+
+
+'''
