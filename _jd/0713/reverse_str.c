949da8efba44ec7ca9597a23759d803c056ca4a9
cnxuan05 <cnxuan05@gmail.com>
2021-07-13 08:21:59 +0800

    add str info

--- _jd/0713/reverse_str.c
+++ _jd/0713/reverse_str.c
@@ -4,15 +4,18 @@
 int main()
 {
 
-    char str[100] = "hello";
-    int max_len = strlen(str)/sizeof(char);
+    unsigned char str[100] = "你好中文";
+    int max_len = strlen(str)/sizeof(unsigned char);
     printf("%d\n", max_len);
 
     for(int j=max_len-1;j>=0;j--)
     {
-        printf("char = %c\n", str[j]);
+        printf("char = %x\n", str[j]);
 
     }
+    int a = 0xe4b0a0;
+
+    printf("%d", a);
 
     return 0;
 }
